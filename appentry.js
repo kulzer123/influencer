5d9510580af6cd6b78811361fe8a66b639724a74
Influencer+ <112989959+kulzer123@users.noreply.github.com>
2023-11-28 19:47:45 -0300

    appentry.js fixed

---
+++ appentry.js
@@ -0,0 +1,20 @@
+[
+name:leilaotestes
+version:1.0.0,
+main:nodemodules/expo/appentry.js
+
+scripts:{
+start: expo -start
+android: expo -start -android
+ios: expo start -ios
+web: expo start-web
+eject: expo -eject.js
+api: json?server -host local.ip -watch db.json
+test=jest
+
+const instance = axios.create ({
+baseURL: http://ip:3000/
+timeout;upload 1000s
+})
+export default instance:
+]
